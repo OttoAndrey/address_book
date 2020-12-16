66c709563b9af3644640a796f4637014390378a0
OttoAndrey <30920379+OttoAndrey@users.noreply.github.com>
2020-12-16 10:30:32 +0700

    Add users viewset

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import viewsets, filters
 
-# Create your views here.
+from users.models import Users
+from users.serializers import UsersSerializer
+
+
+class UsersViewSet(viewsets.ModelViewSet):
+    queryset = Users.objects.all()
+    serializer_class = UsersSerializer
+    filter_backends = [filters.OrderingFilter]
+    ordering_fields = ['full_name', 'avatar', 'gender', 'birth_date', 'living_address']
