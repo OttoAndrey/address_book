31576910484b37bfc6c4d3eeb492eda06d68d5d8
OttoAndrey <30920379+OttoAndrey@users.noreply.github.com>
2020-12-16 10:41:33 +0700

    Add EmailAddress viewset

--- backend/email_address/views.py
+++ backend/email_address/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import viewsets, filters
 
-# Create your views here.
+from email_address.models import EmailAddress
+from email_address.serializers import EmailAddressSerializer
+
+
+class EmailAddressViewSet(viewsets.ModelViewSet):
+    queryset = EmailAddress.objects.all()
+    serializer_class = EmailAddressSerializer
+    filter_backends = [filters.OrderingFilter]
+    ordering_fields = ['email', 'type', 'user']
