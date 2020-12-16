2a0d3266ad6119ec16cabfd9a1f517c91f016dff
OttoAndrey <30920379+OttoAndrey@users.noreply.github.com>
2020-12-16 10:38:35 +0700

    Add EmailAddress model

--- backend/email_address/models.py
+++ backend/email_address/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+from users.models import Users
+
+
+class EmailAddress(models.Model):
+    PERSONAL = 'P'
+    WORK = 'W'
+    TYPES = [
+        (PERSONAL, 'Personal'),
+        (WORK, 'Work')
+    ]
+
+    email = models.EmailField()
+    type = models.CharField(max_length=2, choices=TYPES)
+    user = models.ForeignKey(Users, on_delete=models.CASCADE)
