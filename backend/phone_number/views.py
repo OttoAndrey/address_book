2b8561d0ada5beb0a036c846cad016828642bc48
OttoAndrey <30920379+OttoAndrey@users.noreply.github.com>
2020-12-16 10:38:06 +0700

    Add PhoneNumber viewset

--- backend/phone_number/views.py
+++ backend/phone_number/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import viewsets, filters
 
-# Create your views here.
+from phone_number.models import PhoneNumber
+from phone_number.serializers import PhoneNumberSerializer
+
+
+class PhoneNumberViewSet(viewsets.ModelViewSet):
+    queryset = PhoneNumber.objects.all()
+    serializer_class = PhoneNumberSerializer
+    filter_backends = [filters.OrderingFilter]
+    ordering_fields = ['number', 'type', 'user']
