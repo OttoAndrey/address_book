2e8b65e08c8f826ddeb72a43a8ef4046e4ce8bd8
OttoAndrey <30920379+OttoAndrey@users.noreply.github.com>
2020-12-16 10:28:37 +0700

    Add users model

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Users(models.Model):
+    FEMALE = 'F'
+    MALE = 'M'
+    OTHER = 'O'
+    GENDERS = [
+        (FEMALE, 'Female'),
+        (MALE, 'Male'),
+        (OTHER, 'Other')
+    ]
+
+    full_name = models.CharField(max_length=255)
+    avatar = models.ImageField(null=True, upload_to='avatars')
+    gender = models.CharField(max_length=2, choices=GENDERS)
+    birth_date = models.DateField()
+    living_address = models.TextField()
