5b567870db0cfb8e1ce8e67bd2dafb44a3819326
OttoAndrey <30920379+OttoAndrey@users.noreply.github.com>
2020-12-16 10:37:01 +0700

    Add PhoneNumber model

--- backend/phone_number/models.py
+++ backend/phone_number/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+from users.models import Users
+
+
+class PhoneNumber(models.Model):
+    MOBILE = 'M'
+    CITY = 'C'
+    TYPES = [
+        (MOBILE, 'Mobile'),
+        (CITY, 'City')
+    ]
+
+    number = models.CharField(max_length=11)
+    type = models.CharField(max_length=2, choices=TYPES)
+    user = models.ForeignKey(Users, on_delete=models.CASCADE)
