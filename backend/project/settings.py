6845d4d8fa89c06ad10ada6fa159a65c43eafaa1
OttoAndrey <30920379+OttoAndrey@users.noreply.github.com>
2020-12-17 11:43:55 +0700

    Fix settings.py

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -6,6 +6,7 @@ from environs import Env
 
 load_dotenv()
 env = Env()
+env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
